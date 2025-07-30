0d8a31cfa05a4be484f10d43f44fd67584ecb7d1
Miles Pitassi <mpitassi@MPITAMAC.local>
2025-07-30 09:30:48 -0500

    new

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -2,22 +2,22 @@ export default function Head() {
   return (
     <>
       <title>Miles Pitassi</title>
-      <meta name="description" content="Im Miles, a data analyst and researcher for sports. Im currently a Research Scientist Intern for the Chicago Blackhawks, developing models using the NHLs puck and player tracking data." />
+      <meta name="description" content="I'm Miles, a data analyst and researcher for sports. I'm currently a Research Scientist Intern for the Chicago Blackhawks, developing models using the NHL's puck and player tracking data." />
 
-      {/* Facebook Meta Tags */}
+      {/* Facebook/Open Graph */}
       <meta property="og:url" content="https://miles-pitassi.com" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Miles Pitassi" />
-      <meta property="og:description" content="Im Miles, a data analyst and researcher for sports. Im currently a Research Scientist Intern for the Chicago Blackhawks, developing models using the NHLs puck and player tracking data." />
-      <meta property="og:image" content="https://miles-pitassi.com/social-preview.png">
+      <meta property="og:description" content="I'm Miles, a data analyst and researcher for sports. I'm currently a Research Scientist Intern for the Chicago Blackhawks, developing models using the NHL's puck and player tracking data." />
+      <meta property="og:image" content="https://miles-pitassi.com/social-preview.png" />
 
-      {/* Twitter Meta Tags */}
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="miles-pitassi.com" />
       <meta property="twitter:url" content="https://miles-pitassi.com" />
       <meta name="twitter:title" content="Miles Pitassi" />
-      <meta name="twitter:description" content="Im Miles, a data analyst and researcher for sports. Im currently a Research Scientist Intern for the Chicago Blackhawks, developing models using the NHLs puck and player tracking data." />
-      <meta name="twitter:image" content="https://miles-pitassi.com/social-preview.png">
+      <meta name="twitter:description" content="I'm Miles, a data analyst and researcher for sports. I'm currently a Research Scientist Intern for the Chicago Blackhawks, developing models using the NHL's puck and player tracking data." />
+      <meta name="twitter:image" content="https://miles-pitassi.com/social-preview.png" />
     </>
   );
 }
