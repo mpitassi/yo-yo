bab11f162f7f0bb09e64d3e75ce8bbfb8443a58f
Miles Pitassi <mpitassi@MPITAMAC.local>
2025-07-30 09:21:39 -0500

    new

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -1,15 +1,25 @@
 export default function Head() {
   return (
     <>
-      <title>Miles Pitassi</title>
-      <meta name="description" content="I'm Miles, a data analyst and researcher for sports. I'm currently a Research Scientist Intern for the Chicago Blackhawks, developing models using the NHL's puck and player tracking data." />
-      <meta property="og:title" content="Miles Pitassi" />
-      <meta property="og:description" content="I'm Miles, a data analyst and researcher for sports. I'm currently a Research Scientist Intern for the Chicago Blackhawks." />
-      <meta property="og:image" content="/social-preview.png" />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="/social-preview.png" />
-      <link rel="icon" href="/favicon.ico" />
+<title>Miles Pitassi</title>
+<meta name="description" content="Im Miles, a data analyst and researcher for sports. Im currently a Research Scientist Intern for the Chicago Blackhawks, developing models using the NHLs puck and player tracking data.">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://miles-pitassi.com">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Miles Pitassi">
+<meta property="og:description" content="Im Miles, a data analyst and researcher for sports. Im currently a Research Scientist Intern for the Chicago Blackhawks, developing models using the NHLs puck and player tracking data.">
+<meta property="og:image" content="https://opengraph.b-cdn.net/production/images/59950b30-6e9b-40f0-bdb6-b86b0247795f.png?token=5l-mb2BVFr95s40PatNh6pXRhVPzaEpL1mH7Z6YrJuA&height=864&width=1200&expires=33289885252">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="miles-pitassi.com">
+<meta property="twitter:url" content="https://miles-pitassi.com">
+<meta name="twitter:title" content="Miles Pitassi">
+<meta name="twitter:description" content="Im Miles, a data analyst and researcher for sports. Im currently a Research Scientist Intern for the Chicago Blackhawks, developing models using the NHLs puck and player tracking data.">
+<meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/59950b30-6e9b-40f0-bdb6-b86b0247795f.png?token=5l-mb2BVFr95s40PatNh6pXRhVPzaEpL1mH7Z6YrJuA&height=864&width=1200&expires=33289885252">
+
+<!-- Meta Tags Generated via https://www.opengraph.xyz -->
     </>
   )
 }
